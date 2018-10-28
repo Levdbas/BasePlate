63810e17686a9d7269de87d9037e3eb284a47392
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2018-10-28 19:12:35 +0100

    defer vendor, add version tag to non styles or scripts assets.
    Vendor.js is now defer loaded, moved function to add defer tag to asset.php

--- app/lib/assets.php
+++ app/lib/assets.php
@@ -3,6 +3,9 @@ function get_asset($asset)
 {
     if (file_exists(get_template_directory() . '/dist/')) {
         $file = get_template_directory_uri() . '/dist/' . $asset;
+        if (!strpos($asset, 'styles') || !strpos($asset, 'scripts')):
+            $file .= '?ver=' . wp_get_theme()->Version;
+        endif;
         return $file;
     } else {
         wp_die(__('Draai webpack voor de eerste keer om de dist folder te genereren', 'BasePlate'));
@@ -18,7 +21,7 @@ function scripts_in_footer()
 {
     wp_deregister_script('jquery');
     wp_enqueue_style('BasePlate/css', get_asset('styles/app.css'), false, wp_get_theme()->Version);
-    //wp_enqueue_script('BasePlate/vendor', get_asset('scripts/vendor.js'), false, wp_get_theme()->Version);
+    wp_enqueue_script('BasePlate/vendor', get_asset('scripts/vendor.js'), false, wp_get_theme()->Version);
     wp_enqueue_script('BasePlate/js', get_asset('scripts/app.js'), false, wp_get_theme()->Version);
 }
 
@@ -50,6 +53,20 @@ function baseplate_block_assets()
         array('wp-blocks') // Dependency to include the CSS after it.
     );
 }
+
+function add_async_attribute($tag, $handle)
+{
+    // add script handles to the array below
+    $scripts_to_async = array('BasePlate/js', 'BasePlate/vendor');
+
+    foreach ($scripts_to_async as $async_script) {
+        if ($async_script === $handle) {
+            return str_replace(' src', ' async="async" src', $tag);
+        }
+    }
+    return $tag;
+}
+add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
 add_action('wp_enqueue_scripts', 'scripts_in_footer', 100);
 add_action('enqueue_block_assets', 'baseplate_block_assets');
 add_action('enqueue_block_editor_assets', 'baseplate_editor_assets');
