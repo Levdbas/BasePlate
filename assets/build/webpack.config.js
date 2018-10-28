f90eac2dcfa2133ba6cd61a43a39ae31da207046
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2018-10-28 19:13:14 +0100

    remove unneeded webpack plugin // lazyload cleanup

--- assets/build/webpack.config.js
+++ assets/build/webpack.config.js
@@ -7,7 +7,6 @@ const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
