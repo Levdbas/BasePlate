e2ff22805618db77ee9dd96d039b082dcec0d839
Erik van der Bas - Laptop <erik@basedonline.nl>
2018-11-05 21:43:13 +0100

    mq packer test

--- assets/build/postcss.config.js
+++ assets/build/postcss.config.js
@@ -6,5 +6,6 @@ module.exports = {
     plugins: {
         cssnano: process.env.NODE_ENV === 'production' ? cssnanoConfig : false,
         autoprefixer: process.env.NODE_ENV === 'production' ? true : false,
+        'css-mqpacker': {},
     },
 }
