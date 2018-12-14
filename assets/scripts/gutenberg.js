749315fda3d3c126fb3a58fd20edb4cab72adba6
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2018-12-14 15:41:45 +0100

    gutenberg js fix

--- assets/scripts/gutenberg.js
+++ assets/scripts/gutenberg.js
@@ -1,9 +1,9 @@
-console.log('test')
-function addListBlockClassName(settings, name) {
+console.log('test');
+function addListBlockClassName(settings, block) {
     if (name !== 'core/cover') {
-        return settings
+        return settings;
     }
 
-    console.log(settings)
+    console.log(settings);
 }
-wp.hooks.addFilter('blocks.registerBlockType', 'my-plugin/class-names/list-block', addListBlockClassName)
+wp.hooks.addFilter('blocks.registerBlockType', 'my-plugin/class-names/list-block', addListBlockClassName);
