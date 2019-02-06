edc0154368ce8703d7b334cb8b56f0444693934f
Erik van der Bas - Laptop <erik@basedonline.nl>
2019-02-06 11:52:41 +0100

    browserhappy update

--- app/partials/header-top.php
+++ app/partials/header-top.php
@@ -9,6 +9,6 @@
   <?php get_template_part('partials/header', 'analytics'); ?>
 </head>
 <body <?php body_class(); ?>>
-<!--[if lt IE 7]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
+  <!--[if lte IE 9]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
