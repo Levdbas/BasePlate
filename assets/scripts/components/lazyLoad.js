112534f17f04d221e0181f80609f57a51d833259
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2019-04-20 10:32:50 +0200

    ESlint in the mix

--- assets/scripts/components/lazyLoad.js
+++ assets/scripts/components/lazyLoad.js
@@ -1,6 +1,6 @@
 import LazyLoad from 'vanilla-lazyload/dist/lazyload.js'; // import UMD version for IE8. see: https://github.com/verlok/lazyload/issues/271
 function lazyLoad() {
-    var myLazyLoad = new LazyLoad({
+    new LazyLoad({
         elements_selector: '.lazyload',
         callback_load: function(el) {
             $(el).css('min-height', 'auto');
@@ -12,7 +12,7 @@ function lazyLoad() {
         var img = $(this);
         var width = img.attr('width');
         var height = img.attr('height');
-        var height = img.width() / (width / height);
+        height = img.width() / (width / height);
         $(this).css('min-height', height + 'px');
     });
 }
