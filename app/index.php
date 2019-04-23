76185cc4b949e1e37c052222509bd336ac33b973
Erik van der Bas - Laptop <erik@basedonline.nl>
2019-04-23 16:44:56 +0200

    added classes

--- app/index.php
+++ app/index.php
@@ -19,11 +19,11 @@ get_header(); ?>
     <?php if (have_posts()): ?>
       <?php while (have_posts()):
           the_post(); ?>
-        <article class="">
+        <article class="content">
           <header>
             <h1><?php the_title(); ?></h1>
           </header>
-          <div class="entry-content">
+          <div class="content__entry">
             <?php the_content(); ?>
           </div>
         </article>
