f66d270db9ffc6b913d13759e7b18f221a92ca5d
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2020-03-05 12:03:58 +0100

    add prevent default for clicks inside an ACF block

--- resources/assets/scripts/gutenberg.js
+++ resources/assets/scripts/gutenberg.js
@@ -1,8 +1,11 @@
-import LazyLoad from 'vanilla-lazyload/dist/lazyload.js';
 import lazyLoad from './components/lazyLoad';
 
 var ll = lazyLoad();
 
 acf.addAction('render_block_preview', function($el, attributes) {
     ll.update();
+});
+
+$('body').on('click', '.acf-block-preview a:not( [href^="#"] )', function(e) {
+    e.preventDefault();
 });
