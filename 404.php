02778cbe03e60614052cbab88915c4a49665f396
DESKTOP-8C1CTRA\erikv <erik@basedonline.nl>
2020-03-05 11:09:35 +0100

    correct namespace

--- 404.php
+++ 404.php
@@ -5,6 +5,6 @@
  *
  * @since 2.0.0
  */
-$context = Timber::get_context();
+$context = Timber\Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
-Timber::render('404.twig', $context);
+Timber\Timber::render('404.twig', $context);
